def993015ac28f86571db33b087737e8d6b186b9
Dennis Chan <dennis.chan@oocl.com>
2020-04-15 19:42:57 +0800

    remove employee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -12,17 +12,19 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    public static final String MALE = "male";
+    public static final String FEMALE = "female";
     private List<Employee> employeeList = new ArrayList<>();
-    private int page;
-    private int pageSize;
+
+
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Employee getSpecificEmployee(@PathVariable int id){
-        return employeeList.stream()
+    public ResponseEntity<Employee> getSpecificEmployee(@PathVariable int id){
+        return new ResponseEntity<>(employeeList.stream()
                 .filter(employee -> employee.getId() == id)
                 .findFirst()
-                .orElse(null);
+                .orElse(null), HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping
@@ -36,7 +38,7 @@ public class EmployeeController {
         if(gender != null){
             returnList = returnList.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
             if(returnList.size() == 0){
-                return new ResponseEntity<>(null, HttpStatus.OK);
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
             }
         }
 
@@ -44,7 +46,7 @@ public class EmployeeController {
             try{
                 return new ResponseEntity<>(returnList.subList(page * pageSize, pageSize * pageSize + pageSize), HttpStatus.OK);
             }catch(IndexOutOfBoundsException expcetion){
-                return new ResponseEntity<>(null, HttpStatus.OK);
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
             }
         }
         return new ResponseEntity<>(returnList, HttpStatus.OK);
@@ -64,11 +66,11 @@ public class EmployeeController {
     @PostMapping("/init")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<List<Employee>> createEmployeeForTesting(){
-        employeeList.add(new Employee(0,"Xiaoming", 20, "Male"));
-        employeeList.add(new Employee(1,"Xiaohong", 19, "Female"));
-        employeeList.add(new Employee(2,"Xiaozhi", 15, "Male"));
-        employeeList.add(new Employee(3,"Xiaogang", 16, "Male"));
-        employeeList.add(new Employee(4,"Xiaoxia", 15, "Female"));
+        employeeList.add(new Employee(0,"Xiaoming", 20, MALE));
+        employeeList.add(new Employee(1,"Xiaohong", 19, FEMALE));
+        employeeList.add(new Employee(2,"Xiaozhi", 15, MALE));
+        employeeList.add(new Employee(3,"Xiaogang", 16, MALE));
+        employeeList.add(new Employee(4,"Xiaoxia", 15, FEMALE));
         return new ResponseEntity<>(employeeList, HttpStatus.CREATED);
     }
 
@@ -83,5 +85,56 @@ public class EmployeeController {
         return true;
     }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id,
+                                                   @RequestParam(required = false) String name,
+                                                   @RequestParam(required = false) Integer age,
+                                                   @RequestParam(required = false) String gender,
+                                                   @RequestParam(required = false) Integer salary){
+
+        Employee selectedEmployee = employeeList.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if(selectedEmployee == null){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        if(name != null){
+            selectedEmployee.setName(name);
+        }
+        if(age != null){
+            selectedEmployee.setAge(age);
+        }
+        if(gender != null){
+            if(gender.toUpperCase().equals(MALE.toUpperCase())
+                || gender.toUpperCase().equals(FEMALE.toUpperCase())){
+                selectedEmployee.setGender(gender);
+            }
+        }
+        if(salary != null){
+            selectedEmployee.setSalary(salary);
+        }
+        return new ResponseEntity<>(selectedEmployee, HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Employee> removeEmployee(@PathVariable int id){
+        Employee selectedEmployee = employeeList.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if(selectedEmployee != null){
+            employeeList.remove(selectedEmployee);
+            return new ResponseEntity<>(selectedEmployee, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
+    }
 
 }
